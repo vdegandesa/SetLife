515b66971cbcb64ecd85f9d42afee8c89d33eef9
theprinc <jnhockey19@ufl.edu>
2017-05-24 22:45:18 -0400

    first commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,11 +5,13 @@ import Base from './Base';
 export default class Button extends Base {
     constructor(props) {
         super(props);
+        
     }
+
     render() {
         return (
-            <div className='Button row center'>
-                <p>{this.props.children}</p>
+            <div className='Button row' style={this.props.style}>
+                <div style={{margin:'auto'}}>{this.props.children}</div>
             </div>
         );
     }
