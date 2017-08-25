29f26f3e4f4006a64e49f54a4291294c85c96f00
Victor Lafarga <Victor@Victors-MBP.attlocal.net>
2017-08-25 10:35:04 -0400

    practice

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,10 +3,53 @@ import React from 'react';
 import Base from './Base';
 
 export default class LandingPage extends Base {
+	constructor(props) {
+		super(props);
+		this.autoBind('switchCurrencyToBtc', 'switchCurrencyToDollar')
+		this.state = {
+			selectedCurrency: 'btc'
+		}
+	}
+	switchCurrencyToBtc() {
+		console.log('switchCurrencyToBtc')
+		this.setState({
+			selectedCurrency: 'btc'
+		});
+
+	}
+	switchCurrencyToDollar() {
+		console.log('switchCurrencyToDollar')
+		this.setState({
+			selectedCurrency: 'dollars'
+		});
+
+	}
+
     render() {
+    	console.log('render')
+    	var isDollarSelected;
+
+    	if (this.state.selectedCurrency == 'dollars') {
+    		isDollarSelected = true
+    	}
+    	else {
+    		isDollarSelected = false
+    	}
+
         return (
             <div className='LandingPage'>
-                <h6>LandingPage</h6>
+            	{isDollarSelected
+            		?
+            			<h6>Dollars</h6>
+            		:
+            			<h6>Bitcoin</h6>
+        
+            	}
+                
+                
+                <p onClick={this.switchCurrencyToBtc}>bitcoin Switch</p>
+                <p onClick={this.switchCurrencyToDollar}>dollar Switch</p>
+
             </div>
         );
     }
--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import Route from 'react-router/lib/Route';
 import IndexRoute from 'react-router/lib/IndexRoute';
 
 import App from './components/App';
+import Button from './components/Button';
 import LandingPage from './components/LandingPage';
 
 export default (
     <Route path='/' component={App}>
         <IndexRoute component={LandingPage}/>
+        <Route path='/login' component={Button}/>
     </Route>
 );
